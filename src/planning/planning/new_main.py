37cf4e4c4c7c315f6f5186a006cdc249bffc8fb3
Nicholas <nicholasgellerman@berkeley.edu>
2026-05-11 17:19:31 -0700

    another tuning test

--- src/planning/planning/new_main.py
+++ src/planning/planning/new_main.py
@@ -335,7 +335,7 @@ class LegoBuilder(Node):
             f'pick=({px:.3f},{py:.3f},{pz:.3f}) place=({lx:.3f},{ly:.3f},{lz:.3f})')
 
         pre_grasp = self.ik_planner.compute_ik(
-            self.joint_state, px, py, pz + 0.08, pqx, pqy, pqz, pqw)
+            self.joint_state, px, py, pz + 0.3, pqx, pqy, pqz, pqw)
         if pre_grasp is None:
             self.get_logger().error(f'IK failed: pre-grasp brick {self.brick_idx}')
             return
